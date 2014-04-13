656fe8d69b37e2827a24d0d1723df596bd39ad3f
Puneet Lall <puneetlall@gmail.com>
2014-04-12 23:47:50 -0400

    Changed jni func

--- countIt/src/com/example/countit/CounterJNIWrapper.java
+++ countIt/src/com/example/countit/CounterJNIWrapper.java
@@ -1,9 +1,24 @@
 package com.example.countit;
 
+import java.nio.Buffer;
+import java.util.List;
+
+import android.graphics.Bitmap;
+
 public class CounterJNIWrapper {
 	static {
 		System.loadLibrary("counter-jni");
 	}
 	
-	public static native int processImage();
+	public static int processImage(
+			List<Double> pointsX,
+			List<Double> pointsY,
+			Bitmap input,
+			Bitmap output) {
+		return 0;
+	}
+
+	public static native int processImage(int imgWidth, int imgHeight,
+			int numPoints, Buffer pointBuf, Buffer imgInputBuf,
+			Buffer imgOverlayBuf);
 }
